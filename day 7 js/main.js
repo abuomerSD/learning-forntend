21835ccd8369b60bdabf6977e77e25501818e984
asdf <asdf@asdfs-MacBook-Pro.local>
2023-08-21 19:14:35 +0200

    day 7 js basics

---
+++ day 7 js/main.js
@@ -0,0 +1,46 @@
+// window.alert("test alert");
+
+document.write("test doc");
+
+console.log("test console");
+
+console.error("Error from console");
+
+console.table(["eltayeb", "ahmed" ,"asim"]);
+
+// styling console logs with directive (%c)
+
+console.log("testing console %clog %cstyling", "color: green; font-size: 30px", "color: blue; font-size: 20px");
+
+// an ECMA SCRIPT 6 feature
+myName = "Eltayeb";
+
+console.log(`Hello ${myName}`);
+
+// data types
+
+console.log(typeof "Elatyeb Ibrahim"); //string
+console.log(typeof 500); // number
+console.log(typeof 500.55); // number
+console.log(typeof ["eltayeb", 32, true]); //object
+console.log(typeof {name: "eltayeb", age: 30, salary: 3000}); // object
+console.log(typeof true); // boolean
+console.log(typeof false); // boolean
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+// variables
+
+var studentName = "Eissa";
+
+console.log(studentName);
+
+const a = "Hello form 'dsd'";
+
+console.log(a);
+
+// escape character and line continue
+
+console.log("eltayeb \"ibrahim\"");
+
+
